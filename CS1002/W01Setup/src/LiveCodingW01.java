53435e2a0b9744eafaa8be67c55d5d223681da78
Gabriel Wood <g.wood3712@gmail.com>
2020-09-15 15:34:01 +0100

    removed throws exception

--- CS1002/W01Setup/src/LiveCodingW01.java
+++ CS1002/W01Setup/src/LiveCodingW01.java
@@ -1,5 +1,5 @@
 public class LiveCodingW01 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
