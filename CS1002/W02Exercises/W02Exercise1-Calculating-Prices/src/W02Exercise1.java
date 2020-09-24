803074d4794fa7611052bdf348f4874bc2c071f0
Gabriel Wood <g.wood3712@gmail.com>
2020-09-24 18:21:55 +0100

    Calculated the average price of three burgers from user input

--- CS1002/W02Exercises/W02Exercise1-Calculating-Prices/src/W02Exercise1.java
+++ CS1002/W02Exercises/W02Exercise1-Calculating-Prices/src/W02Exercise1.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class W02Exercise1 {
     public static void main(String[] args) {
-        double burgerPrice = 6.00;
-        System.out.println("The burger costs: £" + burgerPrice);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the prices of the three burgers: ");
+        double burgerPrice = input.nextDouble();
+        double burgerPrice2 = input.nextDouble();
+        double burgerPrice3 = input.nextDouble();
+        System.out.println(
+                "The average price of the three burgers is: £" + (burgerPrice + burgerPrice2 + burgerPrice3) / 3);
 
     }
 }
