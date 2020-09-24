e946a1c015676bb7ce9a854ccefa1e40005676ff
Gabriel Wood <g.wood3712@gmail.com>
2020-09-25 00:47:53 +0100

    Reformatted the exercise to work with the autochecker

--- CS1002/W02Exercises/W02Exercise1-Calculating-Prices/src/W02Exercise1.java
+++ CS1002/W02Exercises/W02Exercise1-Calculating-Prices/src/W02Exercise1.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class W02Exercise1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter the prices of the three burgers: ");
+        System.out.println("Price 1:");
         double burgerPrice = input.nextDouble();
+        System.out.println("Price 2:");
         double burgerPrice2 = input.nextDouble();
+        System.out.println("Price 3:");
         double burgerPrice3 = input.nextDouble();
-        System.out.println(
-                "The average price of the three burgers is: £" + (burgerPrice + burgerPrice2 + burgerPrice3) / 3);
+        System.out.println("The average price is " + (burgerPrice + burgerPrice2 + burgerPrice3) / 3);
 
     }
 }
