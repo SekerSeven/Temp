8cd95352fe289b192ccd8ab7bd8c698bccc1aa6a
Gabriel Wood <g.wood3712@gmail.com>
2020-09-24 18:08:12 +0100

    Declared a variable to store the price of a burger

--- CS1002/W02Exercises/W02Exercise1-Calculating-Prices/src/W02Exercise1.java
+++ CS1002/W02Exercises/W02Exercise1-Calculating-Prices/src/W02Exercise1.java
@@ -1,5 +1,7 @@
 public class W02Exercise1 {
     public static void main(String[] args) {
+        double burgerPrice = 6.00;
+        System.out.println("The burger costs: £" + burgerPrice);
 
     }
 }
